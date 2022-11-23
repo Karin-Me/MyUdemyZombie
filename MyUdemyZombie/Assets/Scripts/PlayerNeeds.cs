43ef455769cb5288f480e26e5504b872f53f2375
Karin-Me <asdotong@gmail.com>
2022-11-23 21:20:22 +0900

    20221123-07
    M10. Player HUD Script Part 2 1부 까지
    14 : 27 초 까지
    
    처음부터 끝까지 실습하였습니다.

--- MyUdemyZombie/Assets/Scripts/PlayerNeeds.cs
+++ MyUdemyZombie/Assets/Scripts/PlayerNeeds.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class PlayerNeeds : MonoBehaviour
 {
@@ -10,12 +11,75 @@ public class PlayerNeeds : MonoBehaviour
     public Need thirst;
     public float NoHungerHPDecay;
     public float noThirstHPDecay;
+    public UnityEvent getDamage;
 
     private void Start()
     {
+        // current value is equal to the start value
+        // 현재값은 시작값과 같습니다.
         health.currentValue = health.startValue;
         hunger.currentValue = hunger.startValue;
         thirst.currentValue = thirst.startValue;
+    }
+
+    private void Update()
+    {
+        // reduce the value over time
+        // 시간이 지남에 따라 값을 줄입니다.
+        hunger.Subtract(hunger.decayRate * Time.deltaTime);
+        thirst.Subtract(thirst.decayRate * Time.deltaTime);
+
+        // check if hunger bar reach zero then
+        //  hunger 막대가 0에 도달하면 
+        if (hunger.currentValue == 0.0f)
+        {
+            // reduce health depend on the value of noHungerHPDecay
+            // noHungerHPDecay 값에 따라 health 감소
+            health.Subtract(NoHungerHPDecay * Time.deltaTime);
+        }
+
+        if (thirst.currentValue == 0.0f)
+        {
+            thirst.Subtract(noThirstHPDecay * Time.deltaTime);
+        }
+        // check if player health reached to zero then call Did funtion
+        // 플레이어의 체력이 0에 도달했는지 확인한 다음 Did 함수를 호출합니다.
+        if (health.currentValue == 0.0f)
+        {
+            Die();
+        }
+
+        // update sliders
+        health.uiSlider.fillAmount = health.GetPercentage();
+        hunger.uiSlider.fillAmount = hunger.GetPercentage();
+        thirst.uiSlider.fillAmount = thirst.GetPercentage();
+    }
+
+    public void Heal(float amount)
+    {
+        health.Add(amount);
+    }
+
+    public void Hunger(float amount)
+    {
+        hunger.Add(amount);
+    }
+
+    public void Thirst(float amount)
+    {
+        thirst.Add(amount);
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        health.Subtract(damageAmount);
+    }
+
+    public void Die()
+    {
+        Debug.Log("Player Died");
+        getDamage?.Invoke();
+
     }
 }
 
