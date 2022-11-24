b2642807cc8cade898f91814a40b5279201c4fb7
Karin-Me <asdotong@gmail.com>
2022-11-25 04:15:52 +0900

    20221125-02
    M15. Object Interaction 까지
    (아이템을 주울수가 있습니다.)
    
    처음부터 끝까지 실습하였습니다.

--- MyUdemyZombie/Assets/Scripts/Items/ItemObject.cs
+++ MyUdemyZombie/Assets/Scripts/Items/ItemObject.cs
@@ -2,8 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemObject : MonoBehaviour
+public class ItemObject : MonoBehaviour, IInteractable
 {
     public ItemData item;
 
+    public string GetInteractPromp()
+    {
+        // show the name of item when our haircross is on the object
+        // 헤어크로스가 오브젝트 위에 있을 때 아이템의 이름을 보여줍니다.
+        return string.Format("pickup {0}", item.ItemName);
+    }
+
+    public void OnInteract()
+    {
+        // destroy game object after interacting with it
+        Destroy(gameObject);
+    }
 }
