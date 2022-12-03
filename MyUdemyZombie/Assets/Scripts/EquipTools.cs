0abe6fd3ca886e2f6404ffe1b41a890965bb68d2
Karin-Me <asdotong@gmail.com>
2022-12-03 22:14:03 +0900

    20221203-02
    M26. Equip Weapon Variable
    
    처음부터 끝까지 실습하였습니다.

--- MyUdemyZombie/Assets/Scripts/EquipTools.cs
+++ MyUdemyZombie/Assets/Scripts/EquipTools.cs
@@ -4,5 +4,36 @@ using UnityEngine;
 
 public class EquipTools : Equip
 {
+    public float attackRate;
+    private bool attacking;
+    public float attackDistance;
+    public bool DoesGatherResources;
+    public bool doesdealDamage;
+    public int damage;
 
+    private Animator itemAnim;
+    private Camera cam;
+
+    private void Awake()
+    {
+        itemAnim = GetComponent<Animator>();
+        cam = Camera.main;
+    }
+
+    public override void OnAttackInput()
+    {
+        attacking = true;
+        itemAnim.SetTrigger("Attack");
+        Invoke("OnCanAttack", attackRate);
+    }
+
+    void OnCanAttack()
+    {
+        attacking = true;
+    }
+
+    public void OnHit()
+    {
+
+    }
 }
